24d0f0841db82e0b9cf52195633ef180d0cba6e5
David <a.kurosawa@gmail.com>
2013-07-14 22:52:22 +0000

    write hi

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
-var express = require('express');
 
-var fs = require('fs');
-
-var app = express.createServer(express.logger());
 
 //var buf = fs.readFileSync("index.html");
 
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
-  response.send("hi");
+  response.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
