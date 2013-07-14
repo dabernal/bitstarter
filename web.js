385a91f58326eae4057090251242f07bf0b43a56
David <a.kurosawa@gmail.com>
2013-07-14 22:48:12 +0000

    write hi

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 var buf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  response.send("hi");
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
